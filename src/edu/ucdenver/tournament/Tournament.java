8d79ab232eafed19d271e466e5398b249b42e9fb
Jmansito <j_mansito@icloud.com>
2022-11-03 12:05:48 -0600

    loadFromFile / saveToFile templates

--- src/edu/ucdenver/tournament/Tournament.java
+++ src/edu/ucdenver/tournament/Tournament.java
@@ -1,8 +1,9 @@
 package edu.ucdenver.tournament;
 
-import java.io.Serializable;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class Tournament implements Serializable {
     private String name;
@@ -19,10 +20,31 @@ public class Tournament implements Serializable {
 
     }
     public void loadFromFile(String fileName){
+        try{
+            File file = new File(fileName);
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String string;
+            while((string = reader.readLine()) != null){
+                // Do something with the file here
+                System.out.println("Doing file stuff");
+            }
+            reader.close();
+        } catch(Exception e){
+            System.out.println("File does not exist");
+            e.printStackTrace();
+        }
 
     }
     public void saveToFile(String fineName){
-
+        String things = "PLACEHOLDER"; // figure out what to write to file
+        try{
+            FileWriter writer = new FileWriter(fineName, false);
+            writer.write(things);
+            writer.close();
+            System.out.println("Successfully overwritten file");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
 
     }
     public void addCountry(String countryName){
