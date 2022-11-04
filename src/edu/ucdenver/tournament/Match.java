1b05b0fe0e6850b1b90bc4df8c93886183babf82
Jmansito <j_mansito@icloud.com>
2022-11-04 16:19:22 -0600

    Updated Match

--- src/edu/ucdenver/tournament/Match.java
+++ src/edu/ucdenver/tournament/Match.java
@@ -1,5 +1,6 @@
 package edu.ucdenver.tournament;
 
+import javax.imageio.IIOException;
 import javax.sound.sampled.Line;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,9 +11,13 @@ public class Match {
     private int scoreTeamB;
     LineUp lineupA;
     LineUp lineupB;
+    Team teamA;
+    Team teamB;
 
     Match(LocalDateTime dateTime, Team teamA, Team teamB){
         this.dateTime=dateTime;
+        this.teamA = teamA;
+        this.teamB = teamB;
         lineupA = new LineUp(teamA);
         lineupB = new LineUp(teamB);
     }
@@ -24,8 +29,18 @@ public class Match {
         return dateTime.isEqual(LocalDateTime.now()) || dateTime.isAfter(LocalDateTime.now());
     }
     public void addPlayer(Player player, Team team){
-
-
+        // team -> Lineup team
+        // player -> player to add
+        // check which team to add to
+        // check if player is on team with LineUp listOfPlayers list
+        // add if not
+        // throw exception if player is in it\
+        LineUp l = new LineUp(team);
+        // check if team A
+        if(l.getTeam() == this.teamA){lineupA.addPlayer(player);}
+        // Check if team B
+        if(l.getTeam() == this.teamB){lineupB.addPlayer(player);}
+        else{throw new IllegalArgumentException("Player is already on team");}
     }
     public List<Referee> getReferees(){
 
