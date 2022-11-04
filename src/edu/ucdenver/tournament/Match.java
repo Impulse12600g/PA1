42538524abcc78b52955f6d9b98cc5d9ece5c003
Jmansito <j_mansito@icloud.com>
2022-11-04 12:37:58 -0600

    match being worked on

--- src/edu/ucdenver/tournament/Match.java
+++ src/edu/ucdenver/tournament/Match.java
@@ -2,6 +2,7 @@ package edu.ucdenver.tournament;
 
 import javax.sound.sampled.Line;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Match {
@@ -10,6 +11,7 @@ public class Match {
     private int scoreTeamB;
     Team teamA;
     Team teamB;
+    private ArrayList<Referee> matchReferees;
 
     Match(LocalDate dateTime, Team teamA, Team teamB){
         this.dateTime=dateTime;
@@ -17,7 +19,7 @@ public class Match {
         this.teamB = teamB;
 
     }
-    public LineUp getTeamA(){
+    public LineUp getTeamA(){ //TODO not sure this is correct
         LineUp lineUp = new LineUp(teamA);
         try{
             lineUp.getTeam();
@@ -27,27 +29,34 @@ public class Match {
         }
 
     }
-    public LineUp getTeamB(){
-
-        return null;//fixme
+    public LineUp getTeamB(){ //TODO not sure this is correct
+        LineUp lineUp = new LineUp(teamB);
+        try{
+            lineUp.getTeam();
+            return lineUp;
+        } catch(Exception e){
+            throw new IllegalArgumentException("Lineup does not exist");
+        }
     }
     public boolean isUpcoming(){
+        // return true if dateTime is today or in the future, false if the date has passed
+        return dateTime.equals(LocalDate.now()) || dateTime.isBefore(LocalDate.now());
 
-        return false;//fixme
     }
     public void addPlayer(Player player, Team team){
-
+        // TODO
+        // Not sure how to implement this yet
+        // uses Lineup to add players from player class?
     }
     public List<Referee> getReferees(){
-
-        return null;//fixme
+        return matchReferees;
     }
     public void addReferee(Referee referee){
-
+        this.matchReferees.add(referee);
     }
     public void setMatchScore(int  scoreTeamA, int scoreTeamB){
         this.scoreTeamA = scoreTeamA;
-        this.scoreTeamB =scoreTeamB;
+        this.scoreTeamB = scoreTeamB;
     }
 
 }
