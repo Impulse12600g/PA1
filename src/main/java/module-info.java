5e36034d505a7e74168c44cdbbfb961c36dcae3f
Impulse12600g <k.lal.1078780@gmail.com>
2022-11-07 16:05:05 -0700

    Change to JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module edu.ucdenver.pa1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+
+    opens edu.ucdenver.app to javafx.fxml;
+    exports edu.ucdenver.app;
+}
